ac5f7d882c5fbee51e1819c403ed5fb88eb3809e
bos3321 <bos33210@naver.com>
2021-08-28 01:56:23 +0900

    Spring-Gradle 연습

--- src/main/java/bigchoi/springproject/repository/MemberRepository.java
+++ src/main/java/bigchoi/springproject/repository/MemberRepository.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> findById(long id);
+    Member save(Member member); //회원 저장
+    Optional<Member> findById(long id); //Optional : JAVA8 기능. FindById가 null값인 경우, null값을 감싸서 반환
     Optional<Member> findByName(String name);
     List<Member> findAll();
 }
--- src/main/java/bigchoi/springproject/repository/MemoryMemberRepository.java
+++ src/main/java/bigchoi/springproject/repository/MemoryMemberRepository.java
@@ -4,20 +4,20 @@ import bigchoi.springproject.domain.Member;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements MemberRepository {
-    private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
+public class MemoryMemberRepository implements MemberRepository { //인터페이스 상속
+    private static Map<Long, Member> store = new HashMap<>(); //domain.member key : long value : member
+    private static long sequence = 0L; //key값 생성
 
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
-        store.put(member.getId(), member);
+        store.put(member.getId(), member); //put(key,value)
         return member;
     }
 
     @Override
     public Optional<Member> findById(long id) {
-        return Optional.ofNullable(store.get(id));
+        return Optional.ofNullable(store.get(id)); //key 값이 null이여도 감쌀 수 있음.
     }
 
     @Override
@@ -29,7 +29,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public List<Member> findAll() {
-        return new ArrayList<>(store.values());
+        return new ArrayList<>(store.values()); //store value값 반환
     }
 
     public void clearStore(){
