2740cd41888c71d9622d497a0866b7d98ea4d75a
bos3321 <bos33210@naver.com>
2021-08-28 00:41:01 +0900

    Spring-Gradle 연습

---
+++ src/main/java/bigchoi/springproject/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package bigchoi.springproject.repository;
+
+import bigchoi.springproject.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
